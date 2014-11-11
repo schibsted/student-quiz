0b2cf514db39c6a9b79daf59b02b811cfa3c383b
Joakim Wånggren <joakim.wanggren@schibsted.se>
2014-11-11 08:52:04 +0100

    Initial skeleton commit of app

---
+++ README.md
@@ -0,0 +1,4 @@
+Student Quiz
+===
+Little app to present a form of questions, and an email field.
+Stores responses in MongoDB.
---
+++ app/index.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    path = require('path');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+
+var server = app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
