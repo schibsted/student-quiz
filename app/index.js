495c3997c0d01301fc09903a222671139a71c57a
Joakim Wånggren <joakim.wanggren@schibsted.se>
2014-11-12 09:02:19 +0100

    Added new relic

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+if (process.env.NODE_ENV === 'production') {
+    require('newrelic');
+}
+
 var express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
